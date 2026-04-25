f9ac2f966f8526bf33b65fd5a14320d2bc8009ab
CoderCat010 <nusraumme@gmail.com>
2026-04-25 10:14:40 +0600

    added practice-1

--- practice-1.js
+++ practice-1.js
@@ -1,9 +1,53 @@
 //----- default function parameters
-function addNum(num1, num2){
-    const result = num1 + num2;
-    console.log(num1, num2, result);
-    
-    return result;
-}
+// function addNum(num1, num2){
+//     const result = num1 + num2;
+//     console.log(num1, num2, result);
+//     return result;
+// }
+// console.log(addNum(3, 2));
 
-console.log(addNum(3, 2));
+
+// function addNum(num1, num2){
+//     const result = num1 + num2;
+//     console.log(num1, num2, result);
+    
+//     return result;
+// }
+// console.log(addNum(3));
+
+
+// function addNum(num1, num2 = 0){
+//     const result = num1 + num2;
+//     console.log(num1, num2, result);
+    
+//     return result;
+// }
+// console.log(addNum(3));
+
+
+// function addNum(num1 = 3, num2 = 0){
+//     const result = num1 + num2;
+//     console.log(num1, num2, result);
+    
+//     return result;
+// }
+// console.log(addNum());
+
+
+// function addNum(num1, num2){
+//     const result = num1 + num2;
+//     console.log(num1, num2, result);
+    
+//     return result;
+// }
+// console.log(addNum());
+
+
+// function addNum(num1 = 99, num2 = 3){
+//     const result = num1 + num2;
+//     console.log(num1, num2, result);
+    
+//     return result;
+// }
+// console.log(addNum(3,2));
+
