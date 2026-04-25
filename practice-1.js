752c22925be756ab15a763372331ab1ff697cc2c
CoderCat010 <nusraumme@gmail.com>
2026-04-25 10:08:42 +0600

    added practice-1

---
+++ practice-1.js
@@ -0,0 +1,9 @@
+//----- default function parameters
+function addNum(num1, num2){
+    const result = num1 + num2;
+    console.log(num1, num2, result);
+    
+    return result;
+}
+
+console.log(addNum(3, 2));
